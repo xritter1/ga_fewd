cc7cac94dbb362c78c28e6728131684f1dc1232f
xritter1 <67650599+xritter1@users.noreply.github.com>
2021-12-22 19:57:32 -0600

    Add files via upload

---
+++ calculator-incomplete/script.js
@@ -0,0 +1,29 @@
+/*html
+buttons- 19 at most, 
+numbers- 0-9, 
+math- =, +, -, *, /, %
+*/
+
+
+/*
+css-
+button- background design, 
+need section to show the math result
+use display grid
+ */
+
+/*
+probably add javascript before design?
+will need addEventListener
+ */
+
+
+
+/*const display = document.querySelector(".display")
+function(num){document.write()} */
+const numOne = document.getElementById("one").addEventListener('click', function() {document.querySelector(".display").innerHTML = 1})
+
+const numTwo = document.getElementById("two").addEventListener('click', function() {document.querySelector(".display").innerHTML = 2})
+
+
+const addition = document.getElementById("plus").addEventListener('click', function() {Math.})
